d7e6702d20d5a37f015e94c1763afdeb374a1e66
Md Sagar Ahammed <sagar2002004@gmail.com>
2024-10-19 19:54:26 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date("Y"); ?>Team SA. Personal Portfolio Maker. All Rights Reserved.</p>
+    </div>
+</footer>
+
+</body>
+</html>
